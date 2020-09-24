82440c2751d839decd6a6bc3c6132e36c10c6716
Sarthak Jha <sarthakjha2001@gmail.com>
2020-09-24 16:19:48 +0530

    minor fixes

--- app.js
+++ app.js
@@ -7,24 +7,23 @@ const user = require('./models/model');
 var app = express();
 app.use(bodyParser.json());
 
-   const testDoc = {
-      name: "Sarthak Jha",
-      branch: "CSE",
-      DOB: "04-04-2001",
-      interest: [
-        "test1", "test2", "test3"
-      ]
-    }
-    user.create(testDoc)
-    .then((user)=> {
-      console.log("user successfully added!");
-    }).catch((err)=> {
-      console.log(`error adding the user: ${err}`)
-    });
+const testDoc = {
+  name: 'Sarthak Jha',
+  branch: 'CSE',
+  DOB: '04-04-2001',
+  interest: ['test1', 'test2', 'test3'],
+};
+user.create(testDoc)
+  .then((user) => {
+    console.log("user successfully added!");
+  }).catch((err) => {
+    console.log(`error adding the user: ${err}`)
+  });
 app.use(detailsRouter);
 
+// error handler
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 module.exports = app;
